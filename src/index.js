88b3a844c7519d650707150d44ecdd6329a3f2e9
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-10-01 19:15:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,63 +2,28 @@ class vDOM {
     virtualDOM = [];
 
     // HTML 5 syntaxe requestemenets
-    SELF_CLOSE = ['base', 'meta', 'link', 'br', 'hr', 'img', 'area', 'input', 'col', 'param', 'wbr'] 
+    SELF_CLOSE = ['base', 'meta', 'link', 'br', 'hr', 'img', 'area', 'input', 'col', 'param', 'wbr', 'embed', 'source', 'track'] 
 
-    childToDOM(idx, tmpDOM) {
-        if (tmpDOM[idx].type == 'text') {
-            return 0;
-        }
-        var totChilds = tmpDOM[idx].opt.childs.length;
+    childAppend(el) {
+        var childs = [];
+        el.childNodes.forEach(el => childs.push(this.childAppend(el)))
 
-        for (var i = 0; i < tmpDOM[idx].opt.childs.length; i++) {
-            totChilds += this.childToDOM(tmpDOM[idx].opt.childs[i], tmpDOM)
-            tmpDOM[idx].opt.childs[i] = tmpDOM[tmpDOM[idx].opt.childs[i]];
-        }
-
-        return totChilds;
+        return { original: el, childs: childs};
     }
     HtmlTovDOM(html = '') {
-        // delete comments 
-        html = html.replace(/<\!\-\-.*\-\->/g, '');
-        // reset DOM
-        this.virtualDOM = [];
+        // parsing 
+        var parser = new DOMParser();
+        html = parser.parseFromString(html, "text/html");
+        // writing DOM
+        this.virtualDOM = { head: [], body: [] };
 
-        // read element by element
-        var tmpContent = '', tmpDOM = [], tmpArgs = [], tree = [];
+        html.body.childNodes.forEach(el => {
+            this.virtualDOM.body.push(this.childAppend(el))
+        })
 
-        for (var i = 0; i < html.length; i++) {
-            tmpContent += html[i];
-            if (html[i] == '<' && html[i + 1] == '!') {
-                // html directives
-
-            }
-        }
-
-        /*
-        ||========================||
-        ||        TEST UNIT       ||
-        ||========================||
-
-        tmpDOM = [
-            { type: 'tag', name: 'div', opt: { args: [], childs: [ 1, 2 ] } }, 
-            { type: 'tag', name: 'p', opt: { args: [], childs: [ 3 ] } }, 
-            { type: 'tag', name: 'div', opt: { args: [], childs: [ 4 ] } }, 
-            { type: 'text', content: 'the text content' }, 
-            { type: 'tag', name: 'b', opt: { args: [], childs: [ 5 ] } }, 
-            { type: 'text', content: 'some bold text' }
-        ]
-        */
-
-        // link
-        for (var i = 0; i < tmpDOM.length; i++) {
-            var inc = this.childToDOM(i, tmpDOM);
-            this.virtualDOM.push(tmpDOM[i]);
-            i += inc;
-        }
-
-        // console.log(this.virtualDOM);
+        console.log(this.virtualDOM)
     }
 }
 
-/*var vdom = new vDOM();
-vdom.HtmlTovDOM();*/
+var vdom = new vDOM();
+vdom.HtmlTovDOM('<div>a<lols>sfdfds</lols>fds</div>');
