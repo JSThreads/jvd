5e7269daa77c66974a81925ff66e03bf3f4e2c8a
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-06-05 09:34:37 +0000

    states and dom done

---
+++ src/dom.js
@@ -0,0 +1,28 @@
+/**
+ * Check a problem with Node destruction 
+ * */
+
+module.exports.JVD = class {
+    // all childs rerender on change without components 
+    // that has not as prop the value that changed
+    Element = class {
+
+    }
+    Root = class {
+
+    }
+    // child component are only rerender if props
+    Component = class {
+
+    }
+    Fragement = class {
+
+    }
+
+    static createRoot(DOMEl, el) {
+
+    }
+    static createElement(tag, props, ...children) {
+
+    }
+}
