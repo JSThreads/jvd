763bc94755dcf5e6b34f1f2563eb85eef18c544a
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-07-02 18:11:47 +0200

    html

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ![image](https://github.com/JSThreads/jvd/assets/73474137/cb25b798-ec43-46a7-8208-17ed2744bca1)
 
-## How it works
+## Changed structure
 
-Jvd is a virtual DOM that has been redesigned to be the most optimal as possible and to rerender as few things as possible with dynamic updates that make changes in the DOM directly without passing threw the virtual DOM. It's possible with the help of `Slice`es that are sub-values in `State`s that can be subscribed dynamicaly as well as unsubscribed, so it's fast and don't pass by the virtual DOM for the smaller changes, and even for arrays or object with states native functions integration the dom is updatable easier and faster.
+- Qwick like states and reactivity system
+- JSX templating
+- Vanilla JSX use
+- CSS interpreter for optimized style rendering
--- src/jvd.js
+++ src/jvd.js
@@ -1,55 +1,31 @@
 class _JVD {
     constructor() {
-        let stateWatcherKey = Symbol('watcher');
-        let stateTargetKey = Symbol('target');
-
-        this.watcher = stateWatcherKey;
-        this.target = stateTargetKey;
-
         class State {
-            constructor(value, dependencices=[], subscribers=[], watchers={}, accessor=[]) {
-                this.value = value;
-                // accessors
+            constructor(value, dependencices=[], watchers={}, accessor=[]) {
+                // the value that is passed in the use State
+                this.value = value; 
+                // dependencies are States that has been used to access a
+                // child value
                 this.dependencices = dependencices;
-                // subscribers
-                this.subscribers = subscribers;
-                // watchers
+                // watchers of the value, child value would call parent update function 
                 this.watchers = watchers;
-                // accessor path
+                // accessor array is the list of accessor used to access your child value
                 this.accessor = accessor;
             }
+            // State accessors
             valueOf() { return this.value; }
             toString() { return this.value.toString(); }
         }
         this.State = State;
 
-        this.useState = (value, dependencices=[], subscribers=[], watchers={}, accessor=[]) => {
+        this.useState = (value, dependencices=[], watchers={}, accessor=[]) => {
             let x = this;
             let y = this.State;
             let z = this.useState;
             
-            return new Proxy(new this.State(value, dependencices, subscribers, watchers, accessor),
-            {
-                get(target, accessor) {
-                    if (accessor == x.watcher) return (w) => {
-                        target.watchers[target.accessor] ? target.watchers[target.accessor].push(w) : target.watchers[target.accessor] = [w];          
-                        return target.value;
-                    }
-                    if (accessor == x.target) return target;
-
-                    if (accessor instanceof y) return z(target.value[accessor], structuredClone([accessor, ...target.dependencices]), target.subscribers, target.watchers, structuredClone([accessor[x.target].toString(), ...target.accessor]));
-                    return z(target.value[accessor], structuredClone(target.dependencices), target.subscribers, target.watchers, structuredClone([accessor.toString(), ...target.accessor]));
-                }
-            });
+            
         }
     }
 }
 
 const JVD = new _JVD();
-
-let l = JVD.useState({ e: { r: { l: 9 } } })
-let q = l[JVD.watcher](9)
-console.log(l.e.r.l)
-console.log(q, l)
-
-// Comparaison of dom to do
